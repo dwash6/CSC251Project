b25052f480dec95da8e1ee1861dd42ff38d18c85
dwash6 <durand.washington@yahoo.com>
2022-04-11 22:19:47 -0400

    Modified to read input from a file

--- Project_DuRand_Washington.java
+++ Project_DuRand_Washington.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.io.*;
 
 public class Project_DuRand_Washington { //begin class
-   public static void main(String[] args) { //begin main method
+   public static void main(String[] args) throws IOException { //begin main method
       
-      Scanner keyboard = new Scanner(System.in); //declare Scanner object for user input
+      File info = new File("PolicyInformation.txt"); //opens PolicyInformation.txt to read it
+      Scanner file = new Scanner(info); //uses the text from PolicyInformaiton.txt as input
       
-      //declare variables from Policy object to hold user input
+      //declare variables from Policy object to hold information from file
       int pNumber;
       String pName;
       
@@ -16,30 +18,26 @@ public class Project_DuRand_Washington { //begin class
       double height;
       double weight;
       
-      //print instructions and collect user input
-      System.out.print("Please enter the Policy Number: ");
-      pNumber = keyboard.nextInt();
-      keyboard.nextLine();
-      
-      System.out.print("Please enter Provider Name: ");
-      pName = keyboard.nextLine();
-      
-      System.out.print("Please enter Policyholder's First Name: ");
-      fName = keyboard.nextLine();
-      System.out.print("Please enter Policyholder's Last Name: ");
-      lName = keyboard.nextLine();
-      
-      System.out.print("Please enter Policyholder's Age: ");
-      age = keyboard.nextInt();
-      keyboard.nextLine();
-      
-      System.out.print("Please enter Policyholder's Smoking Status (smoker/non-smoker): ");
-      smoke = keyboard.nextLine();
-      
-      System.out.print("Please enter Policyholder's Height (in inches): ");
-      height = keyboard.nextDouble();
-      System.out.print("Please enter Policyholder's Weight (in pounds): ");
-      weight = keyboard.nextDouble();
+      //begin while loop to loop through policy holder's information
+      while(file.hasNext()) {
+         //begin assigning values to variables using text from file
+         pNumber = file.nextInt();
+         file.nextLine();
+         pName = file.nextLine();
+         fName = file.nextLine();
+         lName = file.nextLine();
+         age = file.nextInt();
+         file.nextLine();
+         smoke = file.nextLine();
+         height = file.nextDouble();
+         weight = file.nextDouble();
+         if(file.hasNext()) {
+            file.nextLine();//this handles the case where we reach the end of the file - makes sure we don't get an exception
+         }
+         else {
+            break;
+         }
+         file.nextLine();
       
       //build Policy object using constructor with arguments
       Policy p = new Policy(pNumber, pName, fName, lName, age, smoke, height, weight);
@@ -54,7 +52,8 @@ public class Project_DuRand_Washington { //begin class
        System.out.println("Policyholder's Height: " + p.getHeight() + " inches");
        System.out.println("Policyholder's Weight: " + p.getWeight() + " pounds");
        System.out.printf("Policyholder's BMI: %.2f", p.getBMI());
-       System.out.printf("\nPolicy Price: $%,.2f", p.getPolicyPrice());
+       System.out.printf("\nPolicy Price: $%,.2f\n", p.getPolicyPrice());
+     } //end while loop
    
    } //end main method
 } //end class

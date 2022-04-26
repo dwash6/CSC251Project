2be60c18f8688d15ddd345e5d17f46c8027cb618
dwash6 <durand.washington@yahoo.com>
2022-04-25 21:33:56 -0400

    Update Policy.java
    Moved fields and methods to the PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -5,37 +5,19 @@ public class Policy { //begin class
    //user's insurance info
    private int policyNum;
    private String providerName;
-   //user's personal info
-   private String firstName;
-   private String lastName; 
-   private int age;
-   private String smokeStatus;
-   private double height;
-   private double weight;
-   
+
+
    //constructors!
    
    //no-arg constructor
    public Policy() {
       policyNum = 0;
       providerName = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokeStatus = "";
-      height = 0.0;
-      weight = 0.0;
    }
    //constructor that accepts arguments
-   public Policy(int policyNum, String providerName, String firstName, String lastName, int age, String smokeStatus, double height, double weight) {
+   public Policy(int policyNum, String providerName) {
       this.policyNum = policyNum;
       this.providerName = providerName;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-      this.smokeStatus = smokeStatus;
-      this.height = height;
-      this.weight = weight;
    }
    
    //accesser & mutator methods!
@@ -53,42 +35,6 @@ public class Policy { //begin class
    public void setProviderName(String pname) {
       pname = providerName;
    }
-   /* setFirstName(fname) method
-      @param - first name
-   **/
-   public void setFirstName(String fname) {
-      fname = firstName;
-   }
-   /* setLastName(lname) method
-      @param - last name
-   **/
-   public void setLastName(String lname) {
-      lname = lastName;
-   }
-   /* setAge(a) method
-      @param - age
-   **/
-   public void setAge(int a) {
-      a = age;
-   }
-   /* setSmokeStatus(s) method
-      @param - smoke status
-   **/
-   public void setSmokeStatus(String s){
-      s = smokeStatus;
-   }
-   /* setHeight(h) method
-      @param - height
-   **/
-   public void setHeight(double h) {
-      h = height;
-   }
-   /* setWeight(w) method
-      @param - weight
-   **/
-   public void setWeight(double w) {
-      w = weight;
-   }
    
    //getters
    /* getPolicyNumber() method
@@ -103,69 +49,7 @@ public class Policy { //begin class
    public String getProviderName() {
       return providerName;
    }
-   /* getFirstName() method
-      @return - first name
-   **/
-   public String getFirstName() {
-      return firstName;
-   }
-   /* getLastName() method
-      @return - last name
-   **/
-   public String getLastName() {
-      return lastName;
-   }
-   /* getAge() method
-      @return - age
-   **/
-   public int getAge() {
-      return age;
-   }
-   /* getSmokeStatus() method
-      @return - smoke status
-   **/
-   public String getSmokeStatus() {
-      return smokeStatus;
-   }
-   /* getHeight() method
-      @return - height
-   **/
-   public double getHeight() {
-      return height;
-   }
-   /* getWeight() method
-      @return - weight
-   **/
-   public double getWeight() {
-      return weight;
-   }
    
-   /* getBMI() method
-      @return - calculates and returns BMI of the policyholder (user)
-   **/
-   public double getBMI() {
-      return (weight * 703)/(height * height);
-   }
    
-   /* getPolicyPrice() method
-      @return - calculates and returns the price of the insurance policy
-   **/
-   public double getPolicyPrice() {
-      final int BASE_FEE = 600;
-      double price = 0.0;
-      
-      if(age > 50) { //if user is over 50, a fee of $75 will be added to price
-            price = price + 75;
-      }
-      
-      if(smokeStatus.equalsIgnoreCase("smoker")) { //if user is a smoker, a fee of $100 will be added to price
-         price = price + 100;
-      }
-      
-      if(getBMI() > 35) { //if user has a BMI over 35, a calculated fee will be added to price
-         price = ((getBMI() - 35) * 20) + price;
-      }
-      
-      return BASE_FEE + price;
-   }
+   
 } //end class
